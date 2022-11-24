80b01ba3aeaefa4f3dd10ee868b2557f9160b746
qtpeters <quinn.t.peters@gmail.com>
2022-11-24 08:15:11 -0500

    fix: trying to get the CF dist. config to be corret with the S3 bucket

--- bin/aws-cloud-resume.ts
+++ bin/aws-cloud-resume.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { AwsCloudResumeStack } from '../lib/aws-cloud-resume-stack';
+import { CloudResumeStack } from '../lib/aws-cloud-resume-stack';
 
 const app = new cdk.App();
-new AwsCloudResumeStack(app, 'AwsCloudResumeStack', {
+new CloudResumeStack(app, 'AwsCloudResumeStack', {
   env: { 
     account: process.env.CDK_DEFAULT_ACCOUNT, 
     region: process.env.CDK_DEFAULT_REGION 
-  },
+  }
 });
--- lib/aws-cloud-resume-stack.ts
+++ lib/aws-cloud-resume-stack.ts
@@ -2,32 +2,59 @@ import * as cdk from 'aws-cdk-lib';
 import * as path from 'path';
 import { Construct } from 'constructs';
 import { BlockPublicAccess, Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
-import { RemovalPolicy } from 'aws-cdk-lib';
+import { Distribution, AllowedMethods, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { CfnOutput, RemovalPolicy } from 'aws-cdk-lib';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 
-export class AwsCloudResumeStack extends cdk.Stack {
+export class CloudResumeStack extends cdk.Stack {
+
+  // Define a constructor in the class that subclasses cdk.Stack
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+    // Call the userclass with necessary props
     super(scope, id, props);
 
-     // S3 Bucket to hold the website
-    const wsBucket = new Bucket(this, 'WebsiteBucket', {
+    // S3 Bucket to hold the website
+    const wsBucket = new Bucket(this, 'CloudResumeBucket', {
+      publicReadAccess: false,
       blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-      encryption: BucketEncryption.S3_MANAGED,
-      enforceSSL: true,
-      versioned: true,
+      encryption: BucketEncryption.UNENCRYPTED,
+      enforceSSL: false,
+      versioned: false,
+      autoDeleteObjects: true,
+      websiteIndexDocument: "index.html",
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
 
+    // Wrap the web pages up as assets to be uploades
     const asset = path.join(__dirname, 'resume')
 
-    const bucketDeployment = new BucketDeployment(this, 'DeployWebsite', {
+    // Create a bucket deployment to specify the assets 
+    const bucketDeployment = new BucketDeployment(this, 'CloudResumeDeploy', {
       sources: [
         Source.asset(asset)
       ],
       destinationBucket: wsBucket,
-      destinationKeyPrefix: 'resume/',
+      destinationKeyPrefix: '',
     });
+
+
+    // Create a cloudfront distribution to manage https
+    const crDistribution = new Distribution(this, 'CloudResumeDistribution', {
+      defaultBehavior: {
+        origin: new S3Origin(wsBucket),
+        allowedMethods: AllowedMethods.ALLOW_GET_HEAD,
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+    });
+
+    new CfnOutput(
+      this, 'CloudResumeCloudfrontDomainName', { 
+        value:  crDistribution.distributionDomainName
+      }
+    );
 
   }
 }
